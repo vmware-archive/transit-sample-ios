6b3d9e0bc0b286bde7afa422237b960074459b3c
Anisha H & Nitin R <anisha@pivotallabs.com>
2014-08-06 11:17:23 -0400

    JY/JL - added a new table controller for the main view where has the saved items are. The flow is now same as the  android app’s.

---
+++ TTCHackathon/PCFSavedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCFSavedTableViewController.h
+//  TTCHackathon
+//
+//  Created by DX121-XL on 2014-08-06.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCFSavedTableViewController : UITableViewController
+
+@end
--- TTCHackathon/PCFSignInViewController.m
+++ TTCHackathon/PCFSignInViewController.m
@@ -55,7 +55,8 @@ static NSString *const kClientSecret = @"secret";
         
     } else {
         UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
-        UIViewController *controller = [storyboard instantiateViewControllerWithIdentifier:@"PCFDataTableViewController"];
+//        UIViewController *controller = [storyboard instantiateViewControllerWithIdentifier:@"PCFDataTableViewController"];
+        UIViewController *controller = [storyboard instantiateViewControllerWithIdentifier:@"PCFSavedTableViewController"];
         [self.navigationController pushViewController:controller animated:YES];
     }
 }
