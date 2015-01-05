28979d5e0871e3b1e714b693d0c216261988ac52
Joshua Winters and Rob Szumlakowski <pair+jwinters+rszumlakowski@pivotal.io>
2015-01-05 17:07:32 -0500

    Changed to point to demo environment on PWS. Updated version to 1.0.6

--- TTCHackathon/TTCSettings.m
+++ TTCHackathon/TTCSettings.m
@@ -5,19 +5,19 @@
 #import "TTCSettings.h"
 
 // Data service parameters
-NSString *const kOAuthServerURL = @"http://datasync-authentication.demo.vchs.cfms-apps.com";
-NSString *const kDataServiceURL = @"http://datasync-datastore.demo.vchs.cfms-apps.com";
-NSString *const kDataClientID = @"cd68e385-c0e8-4740-a563-748e643a2280";
-NSString *const kDataClientSecret = @"IaioD3Mcj4XU67ySMidiFDNrKwv68RB4Cft2zLrdJHoWcdqjsCSWf1U1EZDR6JKufpNp9NTcBqSxbR6bA95_eg";
+NSString *const kOAuthServerURL = @"http://transit-authz.cfapps.io";
+NSString *const kDataServiceURL = @"http://transit-ds.cfapps.io";
+NSString *const kDataClientID = @"ios-client";
+NSString *const kDataClientSecret = @"006d0cea91f01a82cdc57afafbbc0d26c8328964029d5b5eae920e2fdc703169";
 
 // API Gateway parameters
-NSString *const kRoutePath = @"http://transit-gateway.demo.vchs.cfms-apps.com/ttc/routes";
-NSString *const kStopsPath = @"http://transit-gateway.demo.vchs.cfms-apps.com/ttc/routes/%@";
+NSString *const kRoutePath = @"http://transit-gateway-app.cfapps.io/ttc/routes";
+NSString *const kStopsPath = @"http://transit-gateway-app.cfapps.io/ttc/routes/%@";
 
 // Push service parameters
-NSString *const kPushBaseServerUrl = @"http://push-notifications.demo.vchs.cfms-apps.com";
+NSString *const kPushBaseServerUrl = @"http://transit-push.cfapps.io";
 NSString *const kPushDevelopmentVariantUuid = @"15a001cd-f200-40a1-b052-763fbeee12a3";
 NSString *const kPushDevelopmentVariantSecret = @"84def001-645b-4dfa-af5f-e2659dd27b0f";
 NSString *const kPushProductionVariantUuid = @"211ef0cb-acac-4816-ac46-a401f46ee463";
 NSString *const kPushProductionVariantSecret = @"3a02ec0e-85e3-47c6-8d5b-27ef3a0c386e";
-NSString *const kPushDeviceAlias = @"Transit";
+NSString *const kPushDeviceAlias = @"TransitApp";
