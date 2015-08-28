ba56b882de43541942cb435d0527db443856f874
Dwayne Forde <dwayne.forde@gmail.com>
2015-08-28 13:59:18 -0400

    Updated settings for Borg

--- TTCHackathon/TTCSettings.m
+++ TTCHackathon/TTCSettings.m
@@ -14,7 +14,7 @@ NSString *const kStopsPath = @"http://transit-gateway-app.cfapps.io/ttc/routes/%
 #else
 
 /* API Gateway Parameters */
-NSString *const kRoutePath = @"http://transit-gateway-app.borg.vchs.cfms-apps.com/ttc/routes";
-NSString *const kStopsPath = @"http://transit-gateway-app.borg.vchs.cfms-apps.com/ttc/routes/%@";
+NSString *const kRoutePath = @"https://transit-gateway-app.borg.vchs.cfms-apps.com/ttc/routes";
+NSString *const kStopsPath = @"https://transit-gateway-app.borg.vchs.cfms-apps.com/ttc/routes/%@";
 
 #endif
